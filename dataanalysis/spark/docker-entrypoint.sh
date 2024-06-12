94f9ee2e12de1602c0f1f2ce6c501813717a8ff1
sugiyama404 <mmingriha@gmail.com>
2024-06-12 15:37:09 +0900

    data sync fin

--- dataanalysis/spark/docker-entrypoint.sh
+++ dataanalysis/spark/docker-entrypoint.sh
@@ -7,7 +7,7 @@ function docker_entrypoint() {
   sleep 10
 
   # Digdagサーバの起動
-  digdag server --config /opt/digdag/server.properties &
+  digdag server --config /opt/digdag/server.properties --log-level error &
 
   # Sparkジョブの起動
   spark-submit --packages org.apache.spark:spark-streaming_2.13:3.2.4,org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.apache.spark:spark-avro_2.12:3.2.4 /home/pyspark/streaming.py &
