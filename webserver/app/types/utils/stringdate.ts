3db1ca92c01a5dfeae691653cb02fd108cef99a4
sugiyama404 <mmingriha@gmail.com>
2024-06-17 19:44:01 +0900

    rewrite docker file

--- webserver/app/types/utils/stringdate.ts
+++ webserver/app/types/utils/stringdate.ts
@@ -1,5 +1,9 @@
 //@ts-ignore
 import { Kafka, KafkaConfig } from 'kafkajs';
+//@ts-ignore
+import { options } from '@/api/auth/[...nextauth]/options'
+//@ts-ignore
+import { getServerSession } from "next-auth/next"
 
 export function currentDate(): string {
     const today = new Date();
@@ -19,7 +23,8 @@ export function currentDate(): string {
     return formattedDateStr;
 }
 
-function currentJPDate() {
+async function currentJPDate() {
+    //2024/06/17
     const today = new Date();
     const options: Intl.DateTimeFormatOptions = {
         timeZone: 'Asia/Tokyo',
@@ -32,13 +37,28 @@ function currentJPDate() {
     return dateStr;
 }
 
+async function getUserInfo() {
+    // {
+    //     name: 'alice',
+    //     email: 'alice@example.com',
+    //     sub: '1',
+    //     id: 1,
+    //     iat: 1718501701,
+    //     exp: 1718588101,
+    //     jti: '77df4422-1330-42b3-b267-654d975e8589'
+    //   }
+    const session = await getServerSession(options);
+    const usrinfo: any = session?.user;
+    return usrinfo;
+}
+
 export async function sendMessage(action: string) {
     const kafkaConfig: KafkaConfig = { brokers: ['kafka:9092'], clientId: 'pyspark' }
     const kafka = new Kafka(kafkaConfig)
+    const nowdate = await currentDate()
     const message = {
-        key: `2024-6-17`,
-        value: `{"name": "yuki"}`,
-        action: `${action}`,
+        key: `${nowdate}`,
+        value: `{"name": "yuki" , "action": "${action}", "sendtime": ${Date.now()}}`
     }
     const producer = kafka.producer()
     await producer.connect()
